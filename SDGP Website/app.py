66791bcceafc483311d1e28db9944b3c5d4c93c4
onethdias07 <oneth.20200860@iit.ac.lk>
2023-03-27 13:54:03 +0530

    update

---
+++ SDGP Website/app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+app = Flask(__name__)
+
+model=pickle.load(open('my_model.pkl', 'rb'))
+@app.route('/')
+def hello_world():
+    return render_template("careerFinderSurvey.html")
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    init_features=[int(x) for x in request.form.values()]
+    final = [np.array(init_features)]
+    prediction = model.predict_proba(final)
+
+    if prediction == ("in demand"):
+        return render_template('careerFinder.html',pred="this career is in demand")
+    else :
+        return render_template('careerFinder.html',pred="this career is not in demand")
+
+
+if __name__ == '  main  ':
+    app.run()
