7c9ff4a23acc0ee2d1f509808c9d3c88b000a2fe
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2023-04-01 22:25:54 +0530

    backend to front 2

--- Backend.py
+++ Backend.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from pymongo import *
 import csv
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 cluster = MongoClient("mongodb+srv://CS3:OS18@cluster0.a6pcbds.mongodb.net/?retryWrites=true&w=majority")
 database = cluster["Sdgp_Test"]
 collection = database["career"]
-
+collection_Signup = database["signUp"]
 # with open("CSV_Files/Enterprising.csv", "r") as file:
 #     reader = csv.reader(file)
 #     header = next(reader)
@@ -17,21 +17,39 @@ collection = database["career"]
 #         for i in range(len(header)):
 #             data[header[i]] = row[i]
 #         collection.insert_one(data)
-print("hey")
+
 retreive = collection.find()
 career_list = []
 for documents in retreive:
     career_list.append(documents)
-print("hey")
+
+
 # Showing data to the web page
 @app.route("/career")
 def career():
-    return render_template('Nirusan_CareerDisplay.html', career_list = career_list )
+    return render_template('Nirusan_CareerDisplay.html', career_list=career_list)
+
+
+@app.route('/signup', methods=['GET'])
+def signup_form():
+    return render_template('Nirusan_SignUp.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    user = {
+        'name': name,
+        'email': email,
+        'password': password
+    }
+    collection_Signup.insert_one(user)
+    return 'User signed up!'
 
 
 # 127.0.0.1 is the local machines IP address
 # 5000 port is for TCP
-
-
 if __name__ == "__main__":
     app.run(debug=True)
