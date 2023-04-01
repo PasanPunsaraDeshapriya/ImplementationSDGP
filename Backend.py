d6184d8c274531e37d1d726c675cb796bd2c4b37
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2023-04-01 17:24:18 +0530

    Template folder for fronend

--- Backend.py
+++ Backend.py
@@ -9,28 +9,25 @@ cluster = MongoClient("mongodb+srv://CS3:OS18@cluster0.a6pcbds.mongodb.net/?retr
 database = cluster["Sdgp_Test"]
 collection = database["career"]
 
-with open("CSV_Files/Enterprising.csv", "r") as file:
-    reader = csv.reader(file)
-    header = next(reader)
-    for row in reader:
-        data = {}
-        for i in range(len(header)):
-            data[header[i]] = row[i]
-        collection.insert_one(data)
-
+# with open("CSV_Files/Enterprising.csv", "r") as file:
+#     reader = csv.reader(file)
+#     header = next(reader)
+#     for row in reader:
+#         data = {}
+#         for i in range(len(header)):
+#             data[header[i]] = row[i]
+#         collection.insert_one(data)
+print("hey")
 retreive = collection.find()
 career_list = []
 for documents in retreive:
     career_list.append(documents)
-
+print("hey")
 # Showing data to the web page
-@app.route("/")
-def hello_world():
-    return render_template('home.html', career_list = career_list )
+@app.route("/career")
+def career():
+    return render_template('Nirusan_CareerDisplay.html', career_list = career_list )
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title="About")
 
 # 127.0.0.1 is the local machines IP address
 # 5000 port is for TCP
--- README.md
+++
@@ -1,9 +0,0 @@
-# ImplementationSDGP
-cw2 SDGP implementation
-
-Pasan-7/2/2023-Created Github repo
-
-oneth-7/2/2023-Testing 
-
-Thisara-7/2/2023-Testing
-
