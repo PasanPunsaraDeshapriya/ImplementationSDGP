46b99937868f0edcc374dccd4d727674bfe45c82
onethdias07 <oneth.20200860@iit.ac.lk>
2023-04-11 00:18:08 +0530

    update

--- SDGP Website/JavaScript/university.js
+++ SDGP Website/JavaScript/university.js
@@ -1,29 +1,34 @@
-const selectElement = document.getElementById("provinces");
-const selectedValue = selectElement.value;
-console.log(selectedValue)
-
-const element1 = document.getElementById("foreign");
-const selectedValue1 = element1.value;
-console.log(selectedValue1)
+$(document).ready(function() {
+  $("#provinces").change(function province() {
+    return $(this).val();
+    console.log(selectedValue);
+  });
+});
 
 $(document).ready(function() {
+  $("#foreign").change(function foreign() {
+    const selectedValue1 = $(this).val();
+    console.log(selectedValue1);
+  });
+});
+
+$(document).ready(function size() {
   $("input[name='size']").change(function() {
-    const selectedValue = $("input[name='size']:checked").val();
-    console.log(selectedValue);
+    const selectedValue2 = $("input[name='size']:checked").val();
+    console.log(selectedValue2);
   });
 });
 
-$(document).ready(function() {
+$(document).ready(function program() {
   $("input[name='program']").change(function() {
-    const selectedValue = $("input[name='program']:checked").val();
-    console.log(selectedValue);
+    const selectedValue3 = $("input[name='program']:checked").val();
+    console.log(selectedValue3);
   });
 });
 
-
-$(document).ready(function() {
+$(document).ready(function budget() {
   $("input[name='budget']").change(function() {
-    const selectedValue = $("input[name='budget']:checked").val();
-    console.log(selectedValue);
+    const selectedValue4 = $("input[name='budget']:checked").val();
+    console.log(selectedValue4);
   });
 });
